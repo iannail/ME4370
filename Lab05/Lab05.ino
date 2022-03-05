f113df945f77647addc9840b44f633b91eceda6c
Austin Logan Barber <austinlb001@gmail.com>
2022-03-04 22:46:55 -0600

    Vag

--- Lab05/Lab05.ino
+++ Lab05/Lab05.ino
@@ -9,6 +9,8 @@
  /********* G L O B A L  V A R I A B L E S *********/
  uint16_t valueADC0 = 0; // THE VALUE ON ANANLOG CHANNEL 0
  uint16_t valueADC1 = 0; // THE VALUE ON ANANLOG CHANNEL 1
+ uint8_t servo1 = 25; // HOLDS THE POSITION FOR SERVO 1
+ uint8_t servo2 = 32; // HOLDS THE POSITION FOR SERVO 2
 
 // INCLUDE LIBRARIES
 #include <LiquidCrystal_I2C.h>
@@ -30,7 +32,19 @@ void setup()
     // SET UP ANALOG TO DIGITIAL CONVERSION IN FREE RUNNING MODE
     ADCSRB = 0x00; // 0b00000000
 
-    //  
+    // SET UP PINS FOR OUTPUT
+    DDRB |= 1<<PB5;
+    DDRH |= 1<<PH3;
+    
+    // USE TCNT1  
+    TCCR1A = 0b10000011; // non-inverted mode, 10bit fast pWM
+    TCCR1B = 0b00001100; // (I/O clk)/256 (From prescaler) 
+    
+    // USE TCNT2  
+    TCCR4A = 0b10000011; // non-inverted mode, 10bit fast pWM
+    TCCR4B = 0b00001100; // (I/O clk)/256 (From prescaler) 
+    OCR1A = 40; 
+    OCR4A = 40;  
 
     // INITIALIZE THE LCD SCREEN
     lcd.begin();
@@ -91,14 +105,40 @@ void loop()
         valueADC1 += (ADCH << 8);
     }
 
-    // READ THE INPUT FROM VRy ON ANALOG TO DIGITAL CHANNEL 1
+    // IF THE VALUE ON A1 IS LESS THAN 101, MOVE THE FIRST ARM FORWARD
+    if((valueADC1 < 101) && (servo1 > 25))
+    {
+        servo1 -= 1;
+        OCR1A = servo1;
+    }
+    // IF THE VALUE ON A1 IS GREATER THAN 899, MOVE THE FIRST ARM BACKWARD
+    else if((valueADC1 > 899) && (servo1 < 150))
+    {
+        servo1 += 1;
+        OCR1A = servo1;
+    }
+    
+    // IF THE VALUE ON A0 IS LESS THAN 101, MOVE THE SECOND ARM COUNTER CLOCKWISE
+    if((valueADC0 < 101) && (servo2 > 32))
+    {
+        servo2 -= 1;
+        OCR4A = servo2;
+    }
+    // IF THE VALUE ON A0 IS GREATER THAN 899, MOVE THE SECOND ARM CLOCKWISE
+    else if((valueADC0 > 899) && (servo2 < 150))
+    {
+        servo2 += 1;
+        OCR4A = servo2;
+    }
+    
+
     //ADMUX = 0x41; // 0b01000001
     lcd.clear();
     lcd.setCursor(0, 0);
-    lcd.print("VRx: ");
-    lcd.print(valueADC0);
+    lcd.print("servo1: ");
+    lcd.print(servo1);
     lcd.setCursor(0, 1);
-    lcd.print("VRy: ");
-    lcd.print(valueADC1);
-    delay(250);
+    lcd.print("servo2: ");
+    lcd.print(servo2);
+    delay(10);
 }
