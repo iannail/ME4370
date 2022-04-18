a3a3a09105dd6cbda9597393fc302400178cc288
austinlb001 <austinlb001@gmail.com>
2022-04-18 15:26:41 -0500

    darf

---
+++ Lab06/Lab06.ino
@@ -0,0 +1,127 @@
+// AUTHORS: A. LOGAN BARBER, IAN NAIL
+// FILE NAME: Lab06.ino
+// LAST UPDATED: 17 APRIL 2022
+/*
+ * PURPOSE: THIS FILE IS OUR SOLUTION FOR ME 4370 >> LAB 6.
+ */
+
+/*
+ * STEPPER MOTOR DRIVER:
+ * 
+ * STEPPER MOTOR DRIVER PIN: IN1
+ *     PORT: B 
+ *     PIN: PB0
+ *     DIGITAL PIN: 53
+ *     
+ * STEPPER MOTOR DRIVER PIN: IN2
+ *     PORT: B 
+ *     PIN: PB1
+ *     DIGITAL PIN: 52
+ *     
+ * STEPPER MOTOR DRIVER PIN: IN3
+ *     PORT: B 
+ *     PIN: PB2
+ *     DIGITAL PIN: 51
+ *     
+ * STEPPER MOTOR DRIVER PIN: IN4
+ *     PORT: B 
+ *     PIN: PB3
+ *     DIGITAL PIN: 50
+ */
+
+
+/********** I N C L U D E S **********/
+#include <stdint.h> // ALLOWS TO SPECIFICATION OF THE BIT SIZE OF THE NUMBER
+
+
+/********** D E F I N E S **********/
+#define DELAY 850
+
+
+/********** G L O B A L  V A R I A B L E S **********/
+
+
+// SET UP FUNCTION
+void setup()
+{
+    // SET UP PORT B PINS FOR OUTPUT
+    DDRB = 0x0F; // 0b00001111
+    PORTB = 0x00; // 0b00000000
+}
+
+
+// RUN THIS CODE FOREVER
+void loop()
+{
+    // FORWARD
+    if(// PUT LOGIC HERE)
+    {
+        // TURN ON IN1
+        PORTB |= 0x01; // 0b00000001
+        delayMicroseconds(DELAY);
+    
+        // TURN OFF IN4
+        PORTB &= ~(0x08); // 0b11110111
+        delayMicroseconds(DELAY);
+    
+        // TURN ON IN2
+        PORTB |= 0x02; // 0b00000010
+        delayMicroseconds(DELAY);
+    
+        // TURN OFF IN1
+        PORTB &= ~(0x01); // 0b11111110
+        delayMicroseconds(DELAY);
+    
+        // TURN ON IN3
+        PORTB |= 0x04; // 0b00000100
+        delayMicroseconds(DELAY);
+    
+        // TURN OFF IN2
+        PORTB &= ~(0x02); // 0b11111101
+        delayMicroseconds(DELAY);
+    
+        // TURN ON IN4
+        PORTB |= 0x08; // 0b00001000
+        delayMicroseconds(DELAY);
+    
+        // TURN OFF IN3
+        PORTB &= ~(0x04); // 0b11111011
+        delayMicroseconds(DELAY);
+    }
+    
+    // BACKWARD
+    else if(// PUT LOGIC HERE)
+    {
+         // TURN ON IN4
+        PORTB |= 0x08; // 0b00001000
+        delayMicroseconds(DELAY);
+
+        // TURN ON IN3
+        PORTB |= 0x04; // 0b00000100
+        delayMicroseconds(DELAY);
+
+        // TURN OFF IN4
+        PORTB &= ~(0x08); // 0b11110111
+        delayMicroseconds(DELAY);
+
+        // TURN ON IN2
+        PORTB |= 0x02; // 0b00000010
+        delayMicroseconds(DELAY);
+
+        // TURN OFF IN3
+        PORTB &= ~(0x04); // 0b11111011
+        delayMicroseconds(DELAY);
+         
+         // TURN ON IN1
+        PORTB |= 0x01; // 0b00000001
+        delayMicroseconds(DELAY);
+    
+        // TURN OFF IN2
+        PORTB &= ~(0x02); // 0b11111101
+        delayMicroseconds(DELAY);
+    
+        // TURN OFF IN1
+        PORTB &= ~(0x01); // 0b11111110
+        delayMicroseconds(DELAY);
+    }
+}
